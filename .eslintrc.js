703f9dc0ab5865eb1915a9860b48ebc3a499fcc2
jefersonla <oi_je@hotmail.com>
2016-03-31 15:46:12 -0300

    Added config files for editors

---
+++ .eslintrc.js
@@ -0,0 +1,46 @@
+module.exports = {
+  "env": {
+    "browser": true,
+    "es6": true,
+    "node": true,
+  },
+  "extends": "eslint:recommended",
+  "globals": {
+    "atom": true,
+  },
+  "parserOptions": {
+    "sourceType": "module",
+  },
+  "rules": {
+    "comma-dangle": [
+      2,
+      "only-multiline"
+    ],
+    "curly": [
+      1,
+      "all"
+    ],
+    "indent": [
+      1,
+      2
+    ],
+    "linebreak-style": [
+      2,
+      "unix"
+    ],
+    "no-console": [
+      2,
+      {"allow": ["warn", "error"]},
+    ],
+    "prefer-const": 2,
+    "quotes": [
+      2,
+      "single",
+      "avoid-escape"
+    ],
+    "semi": [
+      2,
+      "always"
+    ],
+  },
+};
